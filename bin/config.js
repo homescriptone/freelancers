cb66c1b08f692eec233add9880ce183b8f159dfc
Killian-debug <58088164+Killian-debug@users.noreply.github.com>
2021-12-22 01:59:49 +0100

    Delete bin directory

--- bin/config.js
+++
@@ -1,8 +0,0 @@
-// consulter https://www.twilio.com/console pour récupérer les informations adéquates
-
-module.exports = {
- accountSid : //accountSID commence par AC  ,
- authToken : // auth token ,
- twilionumber : // numero twilio 
-}
-
